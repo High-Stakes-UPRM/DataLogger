43b7db4e0133e41313b6cf7ec59dcc11814ce466
Daniel Alfaro R <daniel.alfaro@upr.edu>
2021-02-25 18:57:05 -0400

    First Prototype Working

---
+++ HighStakesDatalogger.ino
@@ -0,0 +1,101 @@
+/*
+  SD card datalogger for High Stakes
+
+  modified 2/24/21
+  by Daniel Alfaro R.
+
+*/
+
+#include <SPI.h>
+#include <SD.h>
+#include "DHT.h"
+
+
+#define DHTPIN 2        // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+
+File myFile;
+
+const int chipSelect = 10;
+int dataTake = 1;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  dht.begin();
+  
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+}
+
+void loop() {
+  // make a string for assembling the data to log:
+  String temperatureData = "Temperature is: ";
+  String humidityData = "Humidity is: ";
+  String heatData = "Heat Index is: ";
+
+  // Wait a few seconds between measurements.
+  delay(60000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  temperatureData.concat(t);
+  humidityData.concat(f);
+  heatData.concat(hif);
+
+  String completeData;
+  completeData.concat(dataTake);
+  completeData.concat(" ");
+  completeData.concat(temperatureData);
+  completeData.concat(" ");
+  completeData.concat(humidityData);
+  completeData.concat(" ");
+  completeData.concat(heatData);
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("data.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to data.txt...");
+    myFile.println(completeData);
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening data.txt");
+  }
+  
+  dataTake++;
+
+}
