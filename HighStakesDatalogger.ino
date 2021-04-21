a0092775b38cd33ec975c6f4fb5a966ad7dd3afe
Daniel Alfaro R <daniel.alfaro@upr.edu>
2021-04-21 13:55:44 -0400

    Added SDShield, RTC and barometric sensor

--- HighStakesDatalogger.ino
+++ HighStakesDatalogger.ino
@@ -9,87 +9,117 @@
 #include <SPI.h>
 #include <SD.h>
 #include "DHT.h"
-
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include "RTClib.h"
 
 #define DHTPIN 2        // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11   // DHT 11
 
 File myFile;
-
 const int chipSelect = 10;
-int dataTake = 1;
+SFE_BMP180 pressure;
+RTC_DS1307 rtc;
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  #ifndef ESP8266
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  #endif
 
   dht.begin();
   
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
+  if (!SD.begin(10) || !pressure.begin()) {
+//    Serial.println("initialization failed!");
     while (1);
   }
-  Serial.println("initialization done.");
+  if (! rtc.begin()) {
+    abort();
+  }
+
+  if (! rtc.isrunning()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  
 }
 
 void loop() {
-  // make a string for assembling the data to log:
-  String temperatureData;
-  String humidityData;
-  String heatData;
-
+  char status;
+  double T,P;
   // Wait a few seconds between measurements.
-  delay(2000);
+  delay(3000);
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  DateTime now = rtc.now();
+  String date = String(now.year()) + '/' + String(now.month()) + '/' + String(now.day()) + " " + String(now.hour()) + ':' + String(now.minute()) + ':' + String(now.second());
+
+  //START BMP
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        status = pressure.getPressure(P,T);
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+  //END BMP
+
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
+  if (isnan(h) || isnan(t)) {
+//    Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
-
-  temperatureData.concat(t);
-  humidityData.concat(f);
-  heatData.concat(hif);
-
   String completeData;
-  completeData = String(dataTake) + "," + String(temperatureData) + "," + String(humidityData) + "," + String(heatData);
+  completeData = String(date) + "," + String(t) + "," + String(h) + "," + String(hic) + "," + String(P);
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
+  //Add header row
+  if(!SD.exists("data.csv")){
+    String headerRow = "Time, Temperature (Celsius), Humidity (% RH), Heat Index (Celsius), Pressure (mb)";
+    myFile = SD.open("data.csv", FILE_WRITE);
+  
+    // if the file opened okay, write to it:
+    if (myFile) {
+//      Serial.print("Writing to data.csv...");
+      myFile.println(headerRow);
+      // close the file:
+      myFile.close();
+    } else {
+      // if the file didn't open, print an error:
+//      Serial.println("error opening data.csv for header initialization.");
+    }
+  }
   myFile = SD.open("data.csv", FILE_WRITE);
 
   // if the file opened okay, write to it:
   if (myFile) {
-    Serial.print("Writing to data.csv...");
     myFile.println(completeData);
     // close the file:
     myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening data.csv");
   }
-  
-  dataTake++;
-
 }
